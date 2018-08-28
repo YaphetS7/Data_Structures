7e239a2deae9203ff462f82ad3b2670026da9c95
Daniil <interni17121999@mail.ru>
2018-08-28 06:45:01 +0400

    new

---
+++ LinkedList2/LinkedList2/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList2
+{
+    public class Node
+    {
+        public int value;
+        public Node next, prev;
+        public Node(int x)
+        {
+            value = x;
+            next = null;
+        }
+
+
+    }
+    public class LinkedList2
+    {
+        public Node head;
+        public Node tail;
+        public LinkedList2()
+        {
+            head = null;
+            tail = null;
+        }
+        public void PrintList() // out
+        {
+            Node node;
+            node = head;
+            while (node != null)
+            {
+                Console.Write(node.value + "\t");
+                node = node.next;
+            }
+        }
+
+        public void Add(Node item) // add node to end of list
+        {
+            if (head == null)
+            {
+                head = item;
+                item.prev = null;
+                item.next = null;
+            }
+            else
+            {
+                tail.next = item;
+                item.prev = tail;
+            }
+            tail = item;
+        }
+
+        public Node find(int val) // find node with value = val
+        {
+            Node node;
+            node = head;
+            while (node != null)
+            {
+                if (node.value == val)
+                    return node;
+                node = node.next;
+            }
+            return null;
+        }
+        public void Del(int item) // delete only one node, which element equals item
+        {
+            Node node, temp;
+            node = find(item);
+
+            if (node == head)
+            {
+                temp = node.next;
+                node = null;
+                head = temp;
+                head.prev = null;
+            }
+            else
+               if (node == tail)
+               {
+                   temp = node.prev;
+                   temp.next = null;
+                   tail = temp;
+               }
+            else
+            {
+                node = node.prev;
+                temp = node.next.next;
+                node.next = temp;
+                temp.prev = node;
+            }
+        }
+
+        public void add_after(int x, int item) // add after x(value) item
+        {
+            Node node, newnode, temp;
+            temp = null;
+            node = head;
+            node = find(x);
+            temp = node.next;
+            newnode = new Node(item);
+            node.next = newnode;
+            newnode.next = temp;
+            temp.prev = newnode;
+            newnode.prev = node;
+ 
+        }
+
+        public void AddFirst(int item)
+        {
+            Node node, temp, add;
+            add = new Node(item);
+            temp = null;
+            node = add;
+            node.next = head;
+            node = node.next;
+            node.prev = add;
+            while (node != null)
+            {
+                temp = node;
+                node = node.next;
+                if (node != null)
+                   node.prev = temp;
+            }
+            head = add;
+            tail = temp;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List<int> list = new List<int>();
+                LinkedList2 s1 = new LinkedList2();
+                s1.Add(new Node(5));
+                s1.Add(new Node(30));
+                s1.Add(new Node(15));
+                s1.Add(new Node(20));
+                s1.Add(new Node(10));
+                Console.WriteLine("LinkedList:");
+                s1.PrintList();
+                Console.WriteLine();
+                Console.WriteLine("Add 100 after 30");
+                s1.add_after(30, 100);
+                s1.PrintList();
+                Console.WriteLine();
+                Console.WriteLine("Add zero to first");
+                s1.AddFirst(0);
+                s1.PrintList();
+                Console.WriteLine();
+                Console.WriteLine("Delete 15 from the LinkedList");
+                s1.Del(15);
+                s1.PrintList();
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+namespace Stack
+{
+    class Stack
+    {
+        public int size = 0;
+        ArrayList list = new ArrayList();
+        public void push(object x)
+        {
+            list.Add(x);
+            size++;
+        }
+        public object pop()
+        {
+            object x = list[size - 1];
+            list.RemoveAt(size - 1);
+            size--;
+            return x;
+        }
+    }
+    class Program
+    {
+        public static bool check(string s)
+        {
+            Stack mem = new Stack();
+            mem.push(0);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                    mem.push(0);
+                else
+                    mem.pop();
+                if (mem.size == 0)
+                    return false;
+            }
+            return (mem.size == 1);
+        }
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(); 
+            stack.push(10);
+            stack.push("привет");
+            stack.push(19);
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine("Введите строку скобок:");
+            string a = Console.ReadLine();
+            bool x = check(a);
+            if (x)
+                Console.WriteLine("Строка сбалансированна");
+            else
+                Console.WriteLine("Строка несбалансированна");
+        }
+    }
+}
