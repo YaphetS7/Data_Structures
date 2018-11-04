34a54f24085da2f171dfc91e78998fd8c481c080
Daniil <interni17121999@mail.ru>
2018-11-04 23:16:16 +0400

    04.11.2018

---
+++ BST_In_Array_Sort/BST_In_Array_Sort/Sort_To_BST.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_In_Array_Sort
+{
+   
+    class Sort_To_BST
+    {
+        public static void SortToBTS(int[] arr, int[] result, int start, int end, int k,bool[] check)
+        {
+            if (end >= start)
+            {
+                int mid = (start + end) / 2;
+                result[k] = arr[mid];
+                check[k] = true;
+                SortToBTS(arr,result, start, mid - 1, k * 2 + 1, check);
+                SortToBTS(arr,result, mid + 1, end, k * 2 + 2, check);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int i;
+            int[] a = new int[10];
+            int[] b = new int[15];
+            bool[] check = new bool[15];
+            for (i = 0; i < 10; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Начальный массив: ");
+            Console.WriteLine();
+            for (i = 0; i < 10; i++)
+                Console.Write(a[i] + "\t");
+
+            Array.Sort(a);
+            Console.WriteLine();
+            Console.Write("Отсортированный массив: ");
+            Console.WriteLine();
+            for (i = 0; i < 10; i++)
+                Console.Write(a[i] + "\t");
+
+            Console.WriteLine();
+
+           
+            SortToBTS(a, b, 0, a.Length - 1, 0, check);
+
+            Console.Write("Массив, представленный в BST:");
+            Console.WriteLine();
+            for (i = 0; i < 15; i++)
+                if (check[i])
+                    Console.Write(b[i] + "\t");
+                else
+                    Console.Write("null" + "\t");
+        }
+    }
+}
---
+++ BalancedBST/BalancedBST/BalancedBST.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedBST
+{
+    public class Node
+    {
+        public int value;
+        public Node left, right, parent;
+        public Node(int value)
+        {
+            this.value = value;
+            left = null;
+            right = null;
+            parent = null;
+        }
+    }
+    public class BalancedBSTFromArray
+    {
+        public Node root;
+        public void FromArrayToBST(int[] arr)
+        {
+            Array.Sort(arr);
+            int[] result = new int[arr.Length * 2 + 1];
+            bool[] check = new bool[arr.Length * 2 + 1];
+            SortToBTS(arr, result, 0, arr.Length - 1, 0, check);
+            ArrToBST(result, check, 0);
+        }
+        private Node ArrToBST(int[] arr, bool[] check, int indexOfParent)
+        {
+            if (indexOfParent < arr.Length)
+            {
+                if (indexOfParent == 0)
+                {
+                    root = new Node(arr[indexOfParent]);
+                    root.left = ArrToBST(arr, check, 1);
+                    root.right = ArrToBST(arr, check, 2);
+                    return root;
+                }
+                else
+                if (check[indexOfParent])
+                {
+                    Node node = new Node(arr[indexOfParent]);
+                    node.left = ArrToBST(arr, check, indexOfParent * 2 + 1);
+                    node.right = ArrToBST(arr, check, indexOfParent * 2 + 2);
+                    return node;
+                }
+                else
+                    return null;
+            }
+            else
+                return null;
+        }
+        private void SortToBTS(int[] arr, int[] result, int start, int end, int k, bool[] check)
+        {
+            if (end >= start)
+            {
+                int mid = (start + end) / 2;
+                result[k] = arr[mid];
+                check[k] = true;
+                SortToBTS(arr, result, start, mid - 1, k * 2 + 1, check);
+                SortToBTS(arr, result, mid + 1, end, k * 2 + 2, check);
+            }
+        }
+        public void PrintTree(Node node)
+        {
+            Console.Write(node.value + "\t");
+            if (node.left != null)
+                PrintTree(node.left);
+            if (node.right != null)
+                PrintTree(node.right);
+        }
+    }
+    class BalancedBST
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] {7, 3, 10, 9, 8, 6, 5, 1, 2, 4};
+            BalancedBSTFromArray r = new BalancedBSTFromArray();
+            r.FromArrayToBST(a);
+            r.PrintTree(r.root);
+        }
+    }
+}
