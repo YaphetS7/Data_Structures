832c2768fe3d9b81531fe1e5a9550a3e93999e00
YaphetS7 <interni17121999@mail.ru>
2019-01-30 19:48:56 +0400

    :)

---
+++ Disjoint_Set_Union/Disjoint_Set_Union/DSU.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Disjoint_Set_Union
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class DSU<T>
+    {
+        private Dictionary<T, T> parents = new Dictionary<T, T>();
+        public void MakeSet(T value)
+        {
+            if (!parents.ContainsKey(value))
+                parents.Add(value, value);
+        }
+        public T Find(T value)
+        {
+            if (Convert.ToString(parents[value]) == Convert.ToString(value))
+            {
+                return value;
+            }
+            parents[value] = Find(parents[value]); //эвристика сжатия путей
+            return parents[value];
+
+        }
+        public void Unite(T value1, T value2)
+        {
+            if (!parents.ContainsKey(value1) || !parents.ContainsKey(value2))
+                return;
+            int x = new Random().Next(1, 3);
+            if (x == 1)
+            {
+                parents[value1] = value2;
+            }
+            else
+            {
+                parents[value2] = value1;
+            }
+        }
+    }
+}
