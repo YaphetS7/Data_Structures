342fe6051771fc95f257b2b75080b2db249c59fc
YaphetS7 <interni17121999@mail.ru>
2019-01-30 21:40:48 +0400

    wow

--- Disjoint_Set_Union/Disjoint_Set_Union/DSU.cs
+++ Disjoint_Set_Union/Disjoint_Set_Union/DSU.cs
@@ -7,41 +7,78 @@ namespace Disjoint_Set_Union
     {
         static void Main(string[] args)
         {
-
+          
         }
     }
-
+    public class Node<T>
+    {
+        public T value;
+        public List<Node<T>> values = new List<Node<T>>();
+        public Node<T> parent;
+        public Node(T value)
+        {
+            this.value = value;
+            parent = null;
+        }
+    }
+    public class Set<T>
+    {
+        public Node<T> root;
+        public void AddRoot(Node<T> root)
+        {
+            this.root = root;
+            this.root.parent = root;
+        }
+    }
+   
     public class DSU<T>
     {
-        private Dictionary<T, T> parents = new Dictionary<T, T>();
-        public void MakeSet(T value)
+        public List<Set<T>> list = new List<Set<T>>();
+        public int cntOfSets = 0;
+        public void MakeSet(Node<T> node)
         {
-            if (!parents.ContainsKey(value))
-                parents.Add(value, value);
+            Set<T> newset = new Set<T>();
+            newset.AddRoot(node);
+            list.Add(newset);
+            cntOfSets++;
         }
-        public T Find(T value)
+        public Node<T> Find(Node<T> node)
         {
-            if (Convert.ToString(parents[value]) == Convert.ToString(value))
-            {
-                return value;
-            }
-            parents[value] = Find(parents[value]); //эвристика сжатия путей
-            return parents[value];
-
+            if (node.parent == node)
+                return node;
+            node.parent = Find(node.parent);
+            return node.parent;
         }
-        public void Unite(T value1, T value2)
+        public void Unite(Node<T> value1, Node<T> value2)
         {
-            if (!parents.ContainsKey(value1) || !parents.ContainsKey(value2))
+            Node<T> node1 = Find(value1);
+            Node<T> node2 = Find(value2);
+            if (node1 == node2)
                 return;
             int x = new Random().Next(1, 3);
             if (x == 1)
             {
-                parents[value1] = value2;
+                node1.parent = node2;
+                node2.values.AddRange(node1.values);
+                foreach(Set<T> set in list)
+                    if (set.root == node1)
+                    {
+                        list.Remove(set);
+                        break;
+                    }
             }
             else
             {
-                parents[value2] = value1;
+                node2.parent = node1;
+                node1.values.AddRange(node2.values);
+                foreach (Set<T> set in list)
+                    if (set.root == node2)
+                    {
+                        list.Remove(set);
+                        break;
+                    }
             }
+            cntOfSets--;
         }
     }
 }
