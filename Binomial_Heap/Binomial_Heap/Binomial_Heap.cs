bc96b376ca078e7c884d68f0bfdc628dd409272a
YaphetS7 <interni17121999@mail.ru>
2019-02-04 21:38:06 +0400

    04.02.2019

---
+++ Binomial_Heap/Binomial_Heap/Binomial_Heap.cs
@@ -0,0 +1,103 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binomial_Heap
+{
+    class Binomial_Heap
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Node
+    {
+        public int value, cntOfChilds;
+        public Node left, right, parent;
+        public Node(int value)
+        {
+            this.value = value;
+            left = null;
+            right = null;
+            parent = null;
+            cntOfChilds = 0;
+        }
+    }
+    public class BinomialHeap
+    {
+        public LinkedList<Node> roots = new LinkedList<Node>();
+        public Node[] arr = new Node[100000];
+        public int N;
+        public string Nbit;
+        private int PowerOfTwo(int value)
+        {
+            int k = 0;
+            while(value > 1)
+            {
+                value = value / 2;
+                k++;
+            }
+            return k;
+        }
+        public void Add(Node node)
+        {
+            if (node.cntOfChilds > 0)
+            {
+                AddTree(node);
+                return;
+            }
+            else
+            {
+                if (arr[0] != null)
+                    Merge(node, arr[0], 0);
+                else
+                    arr[0] = node;
+                N += 1;
+                Nbit = Convert.ToString(N, 2);
+            }
+            
+        }
+        private void AddTree(Node node)
+        {
+            int index = PowerOfTwo(node.cntOfChilds + 1);
+            if (index > Nbit.Length || Nbit[index] != 1)
+            {
+                arr[index] = node;
+            }
+            else
+            {
+                Merge(node, arr[index], index);
+            }
+            N += node.cntOfChilds + 1;
+            Nbit = Convert.ToString(N, 2);
+        }
+        private void Merge(Node first, Node second, int index) //index - это индекс массива, элементы которого "сливаются" с другим деревом(или элементом)
+        {
+            if (first.value < second.value)
+                Swap(first, second);
+            Node temp = first.left;
+            first.left = second;
+            second.right = temp;
+            first.cntOfChilds += second.cntOfChilds + 1;
+
+            arr[index] = null;
+            if (arr[index + 1] != null)
+            {
+                Merge(arr[index + 1], first, index + 1);
+            }
+            else
+            {
+                arr[index + 1] = first;
+            }
+        }
+        private void Swap(Node first, Node second)
+        {
+            Node temp = first;
+            first = second;
+            second = temp;
+        }
+    }
+}
---
+++ Binomial_Heap/Binomial_Heap/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Binomial_Heap")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Binomial_Heap")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8d60ef84-bb0a-43af-a5cf-846165eb59c4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
